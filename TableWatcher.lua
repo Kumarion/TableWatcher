b48fb06e8c0d549a3acfff31e9cbffb861842af5
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-01-06 15:38:41 -0500

    Update TableWatcher.lua

--- TableWatcher.lua
+++ TableWatcher.lua
@@ -1,42 +1,42 @@
 local TestTable = { };
 
 local function watch(tabWatching, callback)
-	local _main = { };
+    local _main = { };
 
-	setmetatable(tabWatching, {
-		__index = function(self, key)
+    setmetatable(tabWatching, {
+        __index = function(self, key)
             -- Access key
-			return _main[key];
-		end,
+            return _main[key];
+        end,
 
-		__newindex = function(self, key, value)
-			-- Capture the old value
-			local old_value = rawget(self, key);
-			
+        __newindex = function(self, key, value)
+            -- Capture the old value
+            local old_value = rawget(self, key);
+            
             -- Set callback
-			callback(key, value);
+            callback(key, value);
 
             -- Update old value
-			_main[key] = value;
-		end,
-	});
+            _main[key] = value;
+        end,
+    });
 end
 
 -- Watcher function with a callback of key, test (need to include old value later)
 watch(TestTable, function(key, value)
-	print(key, value);
+    print(key, value);
 end)
 
 -- Test table index 5 with a table of "test" with a table of "test2" with a value of true.
 TestTable["5"] = {
-	test = {
-		test2 = true
-	}
+    test = {
+        test2 = true
+    }
 };
 
 -- Test table index 5 with a table of "test" with a table of "test2" with a value of false.
 TestTable["5"] = {
-	test = {
-		test2 = false
-	}
+    test = {
+        test2 = false
+    }
 };

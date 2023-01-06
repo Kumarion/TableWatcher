44aae5d9bc2c202dbb39c123c77d696b2e0b695a
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-01-06 15:01:01 -0500

    Update TableWatcher.lua

--- TableWatcher.lua
+++ TableWatcher.lua
@@ -1,26 +1,26 @@
 local function SpyOnTable(t)
     -- Create a proxy table to watch the original table
-    local proxy = {}
+    local proxy = { };
 
     local mt = {
-    __index = t,
+        __index = t,
 
-    __newindex = function(t, k, v)
-        print(string.format("Setting t[%s] to %s", tostring(k), tostring(v)))
-        rawset(t, k, v)
-    end
-    }
+        __newindex = function(t, k, v)
+            print(string.format("Setting t[%s] to %s", tostring(k), tostring(v)));
+            rawset(t, k, v);
+        end
+    };
 
     -- Set the metatable to the proxy table
-    setmetatable(proxy, mt)
-    return proxy
+    setmetatable(proxy, mt);
+    return proxy;
 end
 
-local TableToWatch = {}
-TableToWatch = SpyOnTable(watched_table)
+local TableToWatch = {};
+TableToWatch = SpyOnTable(TableToWatch);
 
-TableToWatch.x = 10
-TableToWatch.y = 20
-TableToWatch.z = 30
+TableToWatch.x = 10;
+TableToWatch.y = 20;
+TableToWatch.z = 30;
 
 -- Should have logged all of these changes
